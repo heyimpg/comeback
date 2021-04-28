db5889277a8d9d6cf9b75b1abdc5c922543e4a0d
OOP <tomodachy2000@gmail.com>
2021-04-28 21:02:18 +0700

    Fist Save

---
+++ JVcode/src/firstProgram.java
@@ -0,0 +1,9 @@
+public class firstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello fit");
+        System.out.println("\n\n\n--------End Code--------");
+        System.out.println("Start:" +
+                "6.59" +
+                "");
+    }
+}

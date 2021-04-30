9b3543ac477a04689cb576adbaf0eb70337fe47c
ImPG <65141726+heyimpg@users.noreply.github.com>
2021-04-30 15:33:25 +0700

    Update firstProgram.java

--- JVcode/src/firstProgram.java
+++ JVcode/src/firstProgram.java
@@ -2,6 +2,7 @@ public class firstProgram {
     public static void main(String[] args) {
         System.out.println( "2 3 5 6 a co vk roi");
         System.out.println("Hello fit");
+         System.out.println( "Sua lai tren github");
         System.out.println("\n\n\n--------End Code--------");
         System.out.println("Start:" +
                 "6.59" +

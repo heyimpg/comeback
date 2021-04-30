437d507ac1b06d07b0410afd6fdb3a88a70996d4
OOP <tomodachy2000@gmail.com>
2021-04-30 20:37:43 +0700

    Revise comment and method

--- JVcode/src/firstProgram.java
+++ JVcode/src/firstProgram.java
@@ -1,5 +1,47 @@
 public class firstProgram {
     public static void main(String[] args) {
+
+        onWindows();
+        onGitHub1();
+        onGitHub2();
+
+
+        System.out.println("\n\n\n--------End Code--------");
+    }
+
+    /***
+     * @summary: Code on the GitHub by OOP2
+     * @author: OOP2
+     * @version: 1.0
+     */
+    private static void onGitHub2() {
+        System.out.println("========GitHub2==========");
+        System.out.println( "Sua lai tren client1");
+        System.out.println( "up 2 by client1");
+        System.out.println( "up 3 by client1");
+        System.out.println( "up 4 by client1");
+        System.out.println( "up 5 by client1");
+        System.out.println( "up 6 by client1");
+        System.out.println( "up 8 by client1");
+    }
+
+    /***
+     * @summary: Code on the GitHub by OOP
+     * @author: OOP
+     * @version: 1.0
+     */
+    private static void onGitHub1() {
+        System.out.println("========GitHub==========");
+        System.out.println( "Sua lai tren github");
+        System.out.println( "Sua lai tren github lần 2");
+    }
+
+    /***
+     * @summary: Code on the Windows
+     * @author: OOP
+     * @version: 1.0
+     */
+    private static void onWindows() {
         System.out.println("========Windows==========");
         System.out.println( "2 3 5 6 a co vk roi");
         System.out.println("Hello fit");
@@ -9,23 +51,5 @@ public class firstProgram {
         System.out.println("Push 6 on Windows");
         System.out.println("Push 7 on Windows");
         System.out.println("Push 8 on Windows");
-
-        System.out.println("========GitHub==========");
-        System.out.println( "Sua lai tren github");
-        System.out.println( "Sua lai tren github lần 2");
-        
-        System.out.println("========GitHub2==========");
-        System.out.println( "Sua lai tren client1");
-        System.out.println( "up 2 by client1");
-        System.out.println( "up 3 by client1");
-        System.out.println( "up 4 by client1");
-        System.out.println( "up 5 by client1");
-        System.out.println( "up 6 by client1");
-        System.out.println( "up 8 by client1");
-
-        System.out.println("\n\n\n--------End Code--------");
-        System.out.println("Start:" +
-                "6.59" +
-                "");
     }
 }

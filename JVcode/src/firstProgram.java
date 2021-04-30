97096c29a5c32e9696befa301c38477499dc7269
OOP <tomodachy2000@gmail.com>
2021-04-30 17:39:42 +0700

    Push 7 on windows

--- JVcode/src/firstProgram.java
+++ JVcode/src/firstProgram.java
@@ -7,6 +7,7 @@ public class firstProgram {
         System.out.println("Push 3 on Windows");
         System.out.println("Push 4 on Windows");
         System.out.println("Push 6 on Windows");
+        System.out.println("Push 7 on Windows");
 
         System.out.println("========GitHub==========");
         System.out.println( "Sua lai tren github");

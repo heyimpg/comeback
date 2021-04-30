9f68ef4feef3f4e1f0111233fdfd51e4d3b88ffd
OOP <tomodachy2000@gmail.com>
2021-04-30 16:08:02 +0700

    push1 on Windows

--- JVcode/src/firstProgram.java
+++ JVcode/src/firstProgram.java
@@ -1,9 +1,13 @@
 public class firstProgram {
     public static void main(String[] args) {
+        System.out.println("========Windows==========");
         System.out.println( "2 3 5 6 a co vk roi");
         System.out.println("Hello fit");
-         System.out.println( "Sua lai tren github");
+
+        System.out.println("========GitHub==========");
+        System.out.println( "Sua lai tren github");
         System.out.println( "Sua lai tren github lần 2");
+
         System.out.println("\n\n\n--------End Code--------");
         System.out.println("Start:" +
                 "6.59" +
